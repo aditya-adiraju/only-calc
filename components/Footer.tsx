a201ac6a5faa0c8f6ad73a92cf345e052d5a74e5
Aditya Adiraju <adiraju@student.ubc.ca>
2023-05-20 21:23:26 -0700

    fixed footer overlapping content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import GitHubIcon from '@mui/icons-material/GitHub'
 
 export default function Footer (): React.ReactElement {
   return (
-    <footer className="bg-[#aC9fbb] fixed bottom-0 min-w-full pt-5 min-h-fit max-h-[20%]">
+    <footer className="bg-[#aC9fbb] static bottom-0 min-w-full pt-5">
       <div className="text-lg text-black font-semibold flex justify-center place-items-center min-w-full pb-2">
         Made with ❤️ by Aditya Adiraju
       </div>

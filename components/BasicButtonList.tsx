c2aaa9bb2f97beb1963bef3132600be7808d7fe7
Aditya Adiraju <adiraju@student.ubc.ca>
2023-05-20 17:08:24 -0700

    fixed ESLint issues

--- components/BasicButtonList.tsx
+++ components/BasicButtonList.tsx
@@ -1,45 +1,43 @@
-import { FunctionBody } from "typescript";
-import CalculatorButton from "./CalculatorButton";
-import { RatingClassKey } from "@mui/material";
+import CalculatorButton from './CalculatorButton'
 
 interface BasicButtonListProps {
-  calculation: string | number;
-  addCalculation: (calc: string) => void;
-  clearCalculation: () => void;
-  eraseLastCalculation: () => void;
-  parseCalculation: () => void;
+  calculation: string | number
+  addCalculation: (calc: string) => void
+  clearCalculation: () => void
+  eraseLastCalculation: () => void
+  parseCalculation: () => void
 }
 
-export default function BasicButtonList(props: BasicButtonListProps) {
+export default function BasicButtonList (props: BasicButtonListProps): React.ReactElement {
   return (
     <div className="inline-grid grid-cols-4 gap-3 max-w-md justify-center items-center">
       <CalculatorButton
         className="bg-[#ac9fbb]"
         onClick={props.clearCalculation}
-        value={"C"}
+        value={'C'}
       />
-      <CalculatorButton onClick={props.eraseLastCalculation} value={"⌫"} />
-      <CalculatorButton onClick={() => props.addCalculation("%")} value={"%"} />
-      <CalculatorButton onClick={() => props.addCalculation("*")} value={"X"} />
-      <CalculatorButton onClick={() => props.addCalculation("7")} value={"7"} />
-      <CalculatorButton onClick={() => props.addCalculation("8")} value={"8"} />
-      <CalculatorButton onClick={() => props.addCalculation("9")} value={"9"} />
-      <CalculatorButton onClick={() => props.addCalculation("/")} value={"/"} />
-      <CalculatorButton onClick={() => props.addCalculation("4")} value={"4"} />
-      <CalculatorButton onClick={() => props.addCalculation("5")} value={"5"} />
-      <CalculatorButton onClick={() => props.addCalculation("6")} value={"6"} />
-      <CalculatorButton onClick={() => props.addCalculation("-")} value={"-"} />
-      <CalculatorButton onClick={() => props.addCalculation("1")} value={"1"} />
-      <CalculatorButton onClick={() => props.addCalculation("2")} value={"2"} />
-      <CalculatorButton onClick={() => props.addCalculation("3")} value={"3"} />
-      <CalculatorButton onClick={() => props.addCalculation("+")} value={"+"} />
-      <CalculatorButton onClick={() => props.addCalculation("0")} value={"0"} />
-      <CalculatorButton onClick={() => props.addCalculation(".")} value={"."} />
+      <CalculatorButton onClick={props.eraseLastCalculation} value={'⌫'} />
+      <CalculatorButton onClick={() => { props.addCalculation('%') }} value={'%'} />
+      <CalculatorButton onClick={() => { props.addCalculation('*') }} value={'X'} />
+      <CalculatorButton onClick={() => { props.addCalculation('7') }} value={'7'} />
+      <CalculatorButton onClick={() => { props.addCalculation('8') }} value={'8'} />
+      <CalculatorButton onClick={() => { props.addCalculation('9') }} value={'9'} />
+      <CalculatorButton onClick={() => { props.addCalculation('/') }} value={'/'} />
+      <CalculatorButton onClick={() => { props.addCalculation('4') }} value={'4'} />
+      <CalculatorButton onClick={() => { props.addCalculation('5') }} value={'5'} />
+      <CalculatorButton onClick={() => { props.addCalculation('6') }} value={'6'} />
+      <CalculatorButton onClick={() => { props.addCalculation('-') }} value={'-'} />
+      <CalculatorButton onClick={() => { props.addCalculation('1') }} value={'1'} />
+      <CalculatorButton onClick={() => { props.addCalculation('2') }} value={'2'} />
+      <CalculatorButton onClick={() => { props.addCalculation('3') }} value={'3'} />
+      <CalculatorButton onClick={() => { props.addCalculation('+') }} value={'+'} />
+      <CalculatorButton onClick={() => { props.addCalculation('0') }} value={'0'} />
+      <CalculatorButton onClick={() => { props.addCalculation('.') }} value={'.'} />
       <CalculatorButton
         onClick={props.parseCalculation}
         className="bg-[#ac9fbb] col-span-2"
-        value={"="}
+        value={'='}
       />
     </div>
-  );
+  )
 }
